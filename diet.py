317765b7c6edb06ae40d02295bb80b9a6bb21636
EXPRIVIA\aaprile <alessio.aprile@exprivia.com>
2025-02-27 17:57:26 +0100

    db connection

--- diet.py
+++ diet.py
@@ -1,5 +1,42 @@
 import streamlit as st
 from collections import defaultdict
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Fetch variables
+USER = st.secrets["DB_USERNAME"]
+PASSWORD = st.secrets["DB_PASSWORD"]
+HOST = st.secrets["DB_HOST"]
+PORT = st.secrets["DB_PORT"]
+DBNAME = st.secrets["DB_NAME"]
+
+# Connect to the database
+try:
+    connection = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME
+    )
+    print("Connection successful!")
+    
+    # Create a cursor to execute SQL queries
+    cursor = connection.cursor()
+    
+    # Example query
+    cursor.execute("SELECT NOW();")
+    result = cursor.fetchone()
+    print("Current Time:", result)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+    print("Connection closed.")
+
+except Exception as e:
+    print(f"Failed to connect: {e}")
 
 # Inizializza la lista della spesa nella sessione di Streamlit
 if 'shopping_list' not in st.session_state:
